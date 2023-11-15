efbe25020183a1b8a197e18adbeb843b1822f0e4
Miguel Angel Peña Marin <138605407+DarthKar@users.noreply.github.com>
2023-11-15 13:35:14 -0500

    Update Main.py

--- uiMain/Main.py
+++ uiMain/Main.py
@@ -6,8 +6,16 @@ def CV():
     label_p2.config(text=listaCv[cambio])
     label_p2_fotos.config(image=imgCv[cambio])
 
+def cambioDeVentana():
+    ventana.withdraw()
+    ventanaPrincipalI.deiconify()
+
+def volverVentana():
+    ventanaPrincipalI.withdraw()
+    ventana.deiconify()
+
 ventana = tk.Tk()
-ventana.title("Choope")
+ventana.title("Choopi")
 ventana.geometry("600x500")
 
 # Fotos y textos de CV
@@ -52,7 +60,7 @@ label_p2_fotos.grid(row=4, column=0, pady=10, padx=10)
 
 p3 = tk.Frame(p1, bg="green")
 p3.pack(side="top", padx=10, pady=10, fill="both", expand=True)
-label_p3 = tk.Label(p3, text="¡Bienvenido a Choope!", font=("italic", 16))
+label_p3 = tk.Label(p3, text="¡Bienvenido a Choopi!", font=("italic", 16))
 label_p3.pack(pady=10)
 
 p4 = tk.Frame(p1, bg="green")
@@ -61,7 +69,30 @@ label_foto1 = tk.Label(p4)
 foto1 = tk.PhotoImage(file="src/image1.gif")
 label_foto1.config(image=foto1, compound="bottom")
 label_foto1.pack(pady=10, padx=10)
-botonVentanaUsuario = tk.Button(p4, text="Usuario interfaz", padx=10, pady=10)
+botonVentanaUsuario = tk.Button(p4, text="Usuario interfaz", padx=10, pady=10, command=cambioDeVentana)
 botonVentanaUsuario.pack()
+
+
+
+
+
+################################################################
+
+
+ventanaPrincipalI = tk.Toplevel()
+ventanaPrincipalI.title("choopi")
+ventanaPrincipalI.geometry("600x500")
+menuP=tk.Menu(ventanaPrincipalI)
+
+menuVentanaP= tk.Menu(menuP, font=("italic", 16))
+menuVentanaP.add_cascade(label="Archivo", menu=menuVentanaP)
+menuVentanaP.add_command(label="Procesos y consultas")
+menuVentanaP.add_command(label="Ayuda")
+
+menuArchivo= tk.Menu(menuVentanaP)
+menuArchivo.add_command(label="Aplicacion")
+menuArchivo.add_command(label="Salir")
+ventanaPrincipalI.withdraw()
+
 
 ventana.mainloop()
