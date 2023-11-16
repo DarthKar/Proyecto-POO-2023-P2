7975f2e04eb3157a9442f3fccaa1ef23017a914e
Juan Pablo Gaviria Orozco <141966367+Darkblack595@users.noreply.github.com>
2023-11-15 19:18:24 -0500

    clase producto pasada por completo

--- gestorAplicacion/entidad/Producto/producto.py
+++ gestorAplicacion/entidad/Producto/producto.py
@@ -1,4 +1,7 @@
-import categoria
+from collections import defaultdict
+from baseDatos.Impl.CompradorRepositorio import CompradorRepositorio
+from gestorAplicacion.entidad.Usuario.tiposDeUsuario.comprador.Membresia import Membresia
+
 class Producto:
     def __init__(self,id,nombre,categoria):
         self._id = id
@@ -67,7 +70,118 @@ class Producto:
         def agregarComprador(self,comprador):
             _compradores.append(comprador)
         
-        def agregarResenador(self,resenador):
-            _resenadores.append(resenador)
+        @staticmethod
+        def agregarResenador(cls,resenador):
+            cls._resenadores.append(resenador)
 
-        #Seguir copiando el codigo....
+        @staticmethod
+        def producto_mas_vendido(cls):
+            productos = defaultdict(int)
+
+            for comprador in CompradorRepositorio.obtener():
+                for orden in comprador.get_ordenes():  # Cambiar por transaccion
+                    for producto_transaccion in orden.get_productos_transaccion():
+                        producto = producto_transaccion.get_publicacion().get_producto()
+                        productos[producto] += 1
+
+            producto_mas_vendido = None
+            valor = 0
+
+            for producto, cantidad in productos.items():
+                if cantidad > valor:
+                    valor = cantidad
+                    producto_mas_vendido = producto
+
+            return producto_mas_vendido
+
+        @staticmethod
+        def productoMasCaro(cls):
+            productoMasCaro = 0
+            productoCaro = None
+            for comprador in CompradorRepositorio.obtener():
+                for orden in comprador.get_ordenes():
+                    for productoTransaccion in orden.getProductosTransaccion():
+                        if productoMasCaro < productoTransaccion.getPublicacion().getPrecio():
+                            productoMasCaro = productoTransaccion.getPublicacion().getPrecio()
+                            productoCaro=productoTransaccion.getPublicacion().getProducto().getNombre()
+            return (productoMasCaro+" que vale "+productoCaro)
+        
+        @staticmethod
+        def ventasTotales(cls):
+            valorVentas = 0
+            for comprador in CompradorRepositorio.obtener():
+                for orden in comprador.getOrdenes():
+                    for productoTransaccion in orden.getProductosTransaccion():
+                        valorVentas += (productoTransaccion.getPublicacion().getPrecio()*productoTransaccion.getCantidad())
+            return valorVentas
+
+        @staticmethod
+        def productosTotalesVendidos(cls):
+            valorVendidos = 0
+            for comprador in CompradorRepositorio.obtener():
+                for orden in comprador.getOrdenes():
+                    for productoTransaccion in orden.getProductosTransaccion():
+                        valorVendidos += 1
+            return valorVendidos
+        
+        @staticmethod
+        def productoMasBarato(cls):
+            productoMasBarato = float('inf')
+            productoBarato = None
+
+            for comprador in CompradorRepositorio.obtener():
+                for orden in comprador.get_ordenes():
+                    for productoTransaccion in orden.getProductosTransaccion():
+                        if productoMasBarato > productoTransaccion.getPublicacion().getPrecio():
+                            productoMasBarato = productoTransaccion.getPublicacion().getPrecio()
+                            productoBarato = productoTransaccion.getPublicacion().getProducto().getNombre()
+
+            return f"{productoBarato} que vale {productoMasBarato}"
+
+        @staticmethod
+        def membresia_mas_comprada(cls):
+            Ninguna = 0
+            Basica = 0
+            Bronce = 0
+            Plata = 0
+            Oro = 0         
+            for comprador in CompradorRepositorio.obtener():
+                comparar = comprador.getMembresia()   
+                if comparar == Membresia.NINGUNA : 
+                    Ninguna += 1   
+                elif comparar == Membresia.BASICA :
+                    Basica += 1 
+                elif comparar == Membresia.BRONCE:
+                    Bronce +=1
+                elif comparar == Membresia.PLATA:
+                    Plata += 1
+                elif comparar == Membresia.ORO:
+                    Oro += 1	
+            
+            #Membresia mas comun
+            membresiaMasComun = Membresia.NINGUNA
+            maxCompras = Ninguna
+
+            if (Basica > maxCompras):
+                membresiaMasComun = Membresia.BASICA
+                maxCompras = Basica
+            elif (Bronce > maxCompras):
+                membresiaMasComun = Membresia.BRONCE
+                maxCompras = Bronce
+            elif (Plata > maxCompras):
+                membresiaMasComun = Membresia.PLATA
+                maxCompras = Plata
+            elif (Oro > maxCompras):
+                membresiaMasComun = Membresia.ORO
+	        
+            return membresiaMasComun    
+
+        @staticmethod
+        def cantidadProductosVendidos(cls):
+            productosDiferentes = set()
+            for comprador in CompradorRepositorio.obtener():
+                for orden in comprador.getOrdenes():
+                    for productoTransaccion in orden.getProductosTransaccion():
+                        producto = productoTransaccion.getPublicacion().getProducto()
+                        productosDiferentes.append(producto)
+            return len(productosDiferentes)
