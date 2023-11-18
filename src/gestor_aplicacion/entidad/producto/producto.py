4df3b619b1debaa44fa96b095fc263979b845001
Juan Pablo Gaviria Orozco <141966367+Darkblack595@users.noreply.github.com>
2023-11-18 18:00:00 -0500

    terminacion basedatos y comprador y avanze en funcionalidad 1

--- src/gestor_aplicacion/entidad/producto/producto.py
+++ src/gestor_aplicacion/entidad/producto/producto.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
-from src.base_datos.comprador_repositorio import CompradorRepositorio
-from src.gestor_aplicacion.entidad.usuario.tiposDeUsuario.comprador.membresia import Membresia
+from base_datos.comprador_repositorio import CompradorRepositorio
+from gestor_aplicacion.entidad.usuario.tiposDeUsuario.comprador.membresia import Membresia
+from gestor_aplicacion.entidad.producto.categoria import Categoria
 
 
 class Producto:
@@ -8,183 +9,180 @@ class Producto:
         self._id = id
         self._nombre = nombre
         self._categoria = categoria
-        _opinion = []
-        _compradores = []
-        _publicaciones = []
-        _resenadores = []
+        self._opinion = []
+        self._compradores = []
+        self._publicaciones = []
+        self._resenadores = []
 
-        def addopinionProducto(resena):
-            if resena is not None:
-                _opinion.append(resena)
-        
-        def existeResena(comprador):
-            return comprador in self._resenadores
-        
-        def getId(self):
-            return self.id
-        
-        def getNombre(self):
-            return self.nombre
-        
-        def getCategoria(self):
-            return self.categoria
-        
-        def getopiniones(self):
-            return self._opinion
-        
-        def getPublicaciones(self):
-            return self._publicaciones
-        
-        def agregarPublicacion(self,publicacion):
-            _publicaciones.append(publicacion)
-        
-        def setId(self,id):
-            self.id = id
-        
-        def setNombre(self,nombre):
-            self.nombre = nombre
-        
-        def setCategoria(self,categoria):
-            self.categoria = categoria
-        
-        def setcompradores(self,compradores):
-            self._compradores = compradores
-        
-        def get_compradores(self):
-            return self._compradores
-        
-        def getresenadores(self):
-            return self._resenadores
+    def addopinionProducto(self,resena):
+        if resena is not None:
+           self._opinion.append(resena)
+    
+    def existeResena(self,comprador):
+        return comprador in self._resenadores
+    
+    def getId(self):
+        return self.id
+    
+    def getNombre(self):
+        return self.nombre
+    
+    def getCategoria(self):
+        return self.categoria
+    
+    def getopiniones(self):
+        return self._opinion
+    
+    def getPublicaciones(self):
+        return self._publicaciones
+    
+    def agregarPublicacion(self,publicacion):
+        self._publicaciones.append(publicacion)
+    
+    def setId(self,id):
+        self.id = id
+    
+    def setNombre(self,nombre):
+        self.nombre = nombre
+    
+    def setCategoria(self,categoria):
+        self.categoria = categoria
+    
+    def setcompradores(self,compradores):
+        self._compradores = compradores
+    
+    def get_compradores(self):
+        return self._compradores
+    
+    def getresenadores(self):
+        return self._resenadores
+    
+    def setopinion(self,opinion):
+        self._opinion = opinion
+    
+    def setcompradores(self,compradores):
+        self._compradores = compradores
+    
+    def setresenadores(self,resenadores):
+        self._resenadores = resenadores
+    
+    def is_perecedero(self):
+        return self._categoria.is_perecedero()
+    
+    def agregarComprador(self,comprador):
+        self._compradores.append(comprador)
+    
+    def agregarResenador(self,resenador):
+        self._resenadores.append(resenador)
 
-        def setopinion(self,opinion):
-            self._opinion = opinion
+
+    @staticmethod
+    def producto_mas_vendido(cls):
+        productos = defaultdict(int)
+        for comprador in CompradorRepositorio.obtener():
+            for orden in comprador.get_ordenes():  # Cambiar por transaccion
+                for producto_transaccion in orden.get_productos_transaccion():
+                    producto = producto_transaccion.get_publicacion().get_producto()
+                    productos[producto] += 1
+        producto_mas_vendido = None
+        valor = 0
+        for producto, cantidad in productos.items():
+            if cantidad > valor:
+                valor = cantidad
+                producto_mas_vendido = producto
+        return producto_mas_vendido
+    
+
+    @staticmethod
+    def productoMasCaro(cls):
+        productoMasCaro = 0
+        productoCaro = None
+        for comprador in CompradorRepositorio.obtener():
+            for orden in comprador.get_ordenes():
+                for productoTransaccion in orden.getProductosTransaccion():
+                    if productoMasCaro < productoTransaccion.getPublicacion().getPrecio():
+                        productoMasCaro = productoTransaccion.getPublicacion().getPrecio()
+                        productoCaro=productoTransaccion.getPublicacion().getProducto().getNombre()
+        return (productoMasCaro+" que vale "+productoCaro)
+    
+    @staticmethod
+    def ventasTotales(cls):
+        valorVentas = 0
+        for comprador in CompradorRepositorio.obtener():
+            for orden in comprador.getOrdenes():
+                for productoTransaccion in orden.getProductosTransaccion():
+                    valorVentas += (productoTransaccion.getPublicacion().getPrecio()*productoTransaccion.getCantidad())
+        return valorVentas
+    
+
+    @staticmethod
+    def productosTotalesVendidos(cls):
+        valorVendidos = 0
+        for comprador in CompradorRepositorio.obtener():
+            for orden in comprador.getOrdenes():
+                for productoTransaccion in orden.getProductosTransaccion():
+                    valorVendidos += 1
+        return valorVendidos
+    
+    @staticmethod
+    def productoMasBarato(cls):
+        productoMasBarato = float('inf')
+        productoBarato = None
+        for comprador in CompradorRepositorio.obtener():
+            for orden in comprador.get_ordenes():
+                for productoTransaccion in orden.getProductosTransaccion():
+                    if productoMasBarato > productoTransaccion.getPublicacion().getPrecio():
+                        productoMasBarato = productoTransaccion.getPublicacion().getPrecio()
+                        productoBarato = productoTransaccion.getPublicacion().getProducto().getNombre()
+        return f"{productoBarato} que vale {productoMasBarato}"
+    
+
+    @staticmethod
+    def membresia_mas_comprada(cls):
+        Ninguna = 0
+        Basica = 0
+        Bronce = 0
+        Plata = 0
+        Oro = 0         
+        for comprador in CompradorRepositorio.obtener():
+            comparar = comprador.getMembresia()   
+            if comparar == Membresia.NINGUNA : 
+                Ninguna += 1   
+            elif comparar == Membresia.BASICA :
+                Basica += 1 
+            elif comparar == Membresia.BRONCE:
+                Bronce +=1
+            elif comparar == Membresia.PLATA:
+                Plata += 1
+            elif comparar == Membresia.ORO:
+                Oro += 1	
         
-        def setcompradores(self,compradores):
-            self._compradores = compradores
-        
-        def setresenadores(self,resenadores):
-            self._resenadores = resenadores
-        
-        def is_perecedero(self):
-            return self.categoria.perecedero
+        #Membresia mas comun
+        membresiaMasComun = Membresia.NINGUNA
+        maxCompras = Ninguna
+        if (Basica > maxCompras):
+            membresiaMasComun = Membresia.BASICA
+            maxCompras = Basica
+        elif (Bronce > maxCompras):
+            membresiaMasComun = Membresia.BRONCE
+            maxCompras = Bronce
+        elif (Plata > maxCompras):
+            membresiaMasComun = Membresia.PLATA
+            maxCompras = Plata
+        elif (Oro > maxCompras):
+            membresiaMasComun = Membresia.ORO
+	    
+        return membresiaMasComun    
+    
 
-        def agregarComprador(self,comprador):
-            _compradores.append(comprador)
-        
-        @staticmethod
-        def agregarResenador(cls,resenador):
-            cls._resenadores.append(resenador)
-
-        @staticmethod
-        def producto_mas_vendido(cls):
-            productos = defaultdict(int)
-
-            for comprador in CompradorRepositorio.obtener():
-                for orden in comprador.get_ordenes():  # Cambiar por transaccion
-                    for producto_transaccion in orden.get_productos_transaccion():
-                        producto = producto_transaccion.get_publicacion().get_producto()
-                        productos[producto] += 1
-
-            producto_mas_vendido = None
-            valor = 0
-
-            for producto, cantidad in productos.items():
-                if cantidad > valor:
-                    valor = cantidad
-                    producto_mas_vendido = producto
-
-            return producto_mas_vendido
-
-        @staticmethod
-        def productoMasCaro(cls):
-            productoMasCaro = 0
-            productoCaro = None
-            for comprador in CompradorRepositorio.obtener():
-                for orden in comprador.get_ordenes():
-                    for productoTransaccion in orden.getProductosTransaccion():
-                        if productoMasCaro < productoTransaccion.getPublicacion().getPrecio():
-                            productoMasCaro = productoTransaccion.getPublicacion().getPrecio()
-                            productoCaro=productoTransaccion.getPublicacion().getProducto().getNombre()
-            return (productoMasCaro+" que vale "+productoCaro)
-        
-        @staticmethod
-        def ventasTotales(cls):
-            valorVentas = 0
-            for comprador in CompradorRepositorio.obtener():
-                for orden in comprador.getOrdenes():
-                    for productoTransaccion in orden.getProductosTransaccion():
-                        valorVentas += (productoTransaccion.getPublicacion().getPrecio()*productoTransaccion.getCantidad())
-            return valorVentas
-
-        @staticmethod
-        def productosTotalesVendidos(cls):
-            valorVendidos = 0
-            for comprador in CompradorRepositorio.obtener():
-                for orden in comprador.getOrdenes():
-                    for productoTransaccion in orden.getProductosTransaccion():
-                        valorVendidos += 1
-            return valorVendidos
-        
-        @staticmethod
-        def productoMasBarato(cls):
-            productoMasBarato = float('inf')
-            productoBarato = None
-
-            for comprador in CompradorRepositorio.obtener():
-                for orden in comprador.get_ordenes():
-                    for productoTransaccion in orden.getProductosTransaccion():
-                        if productoMasBarato > productoTransaccion.getPublicacion().getPrecio():
-                            productoMasBarato = productoTransaccion.getPublicacion().getPrecio()
-                            productoBarato = productoTransaccion.getPublicacion().getProducto().getNombre()
-
-            return f"{productoBarato} que vale {productoMasBarato}"
-
-        @staticmethod
-        def membresia_mas_comprada(cls):
-            Ninguna = 0
-            Basica = 0
-            Bronce = 0
-            Plata = 0
-            Oro = 0         
-            for comprador in CompradorRepositorio.obtener():
-                comparar = comprador.getMembresia()   
-                if comparar == Membresia.NINGUNA : 
-                    Ninguna += 1   
-                elif comparar == Membresia.BASICA :
-                    Basica += 1 
-                elif comparar == Membresia.BRONCE:
-                    Bronce +=1
-                elif comparar == Membresia.PLATA:
-                    Plata += 1
-                elif comparar == Membresia.ORO:
-                    Oro += 1	
-            
-            #Membresia mas comun
-            membresiaMasComun = Membresia.NINGUNA
-            maxCompras = Ninguna
-
-            if (Basica > maxCompras):
-                membresiaMasComun = Membresia.BASICA
-                maxCompras = Basica
-            elif (Bronce > maxCompras):
-                membresiaMasComun = Membresia.BRONCE
-                maxCompras = Bronce
-            elif (Plata > maxCompras):
-                membresiaMasComun = Membresia.PLATA
-                maxCompras = Plata
-            elif (Oro > maxCompras):
-                membresiaMasComun = Membresia.ORO
-	        
-            return membresiaMasComun    
-
-        @staticmethod
-        def cantidadProductosVendidos(cls):
-            productosDiferentes = set()
-            for comprador in CompradorRepositorio.obtener():
-                for orden in comprador.getOrdenes():
-                    for productoTransaccion in orden.getProductosTransaccion():
-                        producto = productoTransaccion.getPublicacion().getProducto()
-                        productosDiferentes.append(producto)
-            return len(productosDiferentes)
+    @staticmethod
+    def cantidadProductosVendidos(cls):
+        productosDiferentes = set()
+        for comprador in CompradorRepositorio.obtener():
+            for orden in comprador.getOrdenes():
+                for productoTransaccion in orden.getProductosTransaccion():
+                    producto = productoTransaccion.getPublicacion().getProducto()
+                    productosDiferentes.append(producto)
+        return len(productosDiferentes)
 
 
---
+++ src/uiMain/field_frame_comprador_principal.py
@@ -0,0 +1,70 @@
+import tkinter as tk
+from tkinter import messagebox
+
+class field_frame_comprador_principal(tk.Frame):
+    def __init__(self, master, tituloCriterio, nombres_criterios, cantidad_campos, tituloValores, valores=None, habilitado=None):
+        super().__init__(master, width=500, height=500)
+        
+        self.tituloCriterio = tituloCriterio
+        self.cantidad_campos = cantidad_campos
+        self.tituloValores = tituloValores
+        self.nombres_criterios = nombres_criterios
+        self.valores = valores
+        self.habilitado = habilitado
+        self.pulsado = False
+        self.Valores = None
+        self.pack()
+
+        if self.habilitado is None:
+            self.habilitado = [False] * self.cantidad_campos
+
+        if self.valores is None:
+            self.valores = [""] * self.cantidad_campos
+
+        label_titulo_criterio = tk.Label(self, text=self.tituloCriterio, font=("Arial", 10))
+        label_titulo_criterio.grid(row=0, column=0, columnspan=6, sticky="w")
+
+        label_titulo_valores = tk.Label(self, text=self.tituloValores, font=("Arial", 10))
+        label_titulo_valores.grid(row=0, column=6, columnspan=6, sticky="w")
+
+        for i in range(self.cantidad_campos):
+            label_criterio = tk.Label(self, text=f"{self.nombres_criterios[i]}")
+            label_criterio.grid(row=i + 1, column=0, columnspan=6, sticky="w")
+
+            entry_value = tk.StringVar()
+            entry_value.set(str(i + 1))
+            entry = tk.Entry(self, state="readonly",textvariable=entry_value,justify='center')
+            entry.insert(0, self.valores[i])
+            entry.grid(row=i + 1, column=6, columnspan=6, sticky="w")
+        
+        
+        self.entrada_usuario = tk.Entry(self,state="normal",justify='center')
+        self.entrada_usuario.insert(0,"Ingrese un numero")
+        self.entrada_usuario.grid(row=self.cantidad_campos + 2, column=0, columnspan=6, padx=10, pady=10, sticky="w")
+
+        self.boton_aceptar = tk.Button(self, text="enviar", command=self.Opciones)
+        self.boton_aceptar.grid(row=self.cantidad_campos + 2, column=6, columnspan=6, padx=10, pady=10, sticky="w")
+
+    def Opciones(self):
+        elegido = self.entrada_usuario.get()
+        def validar_entrada():
+            try:
+                opcion = int(elegido)
+                if (opcion < 1) or (opcion > 12):
+                    raise ValueError("El número debe estar entre 1 y 12")        #Bloque de excepcion de numero          
+            except ValueError:
+                if elegido.isdigit():
+                    messagebox.showinfo("Cuidado!","Ingrese un número válido entre 1 y 12")            
+                    return False
+                if elegido == "":
+                    messagebox.showinfo("Cuidado!","Ingrese un número válido")
+                    return False
+                else:
+                    messagebox.showinfo("Cuidado!","Ingrese un número válido (no letras)")
+                    return False
+            return True
+        if not validar_entrada():
+            return                                                          
+        #---------------------------------------------------------------
+        
+            
