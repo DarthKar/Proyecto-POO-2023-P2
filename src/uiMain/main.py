3bf9937b5010b927150199b297a9e5396e941726
segomezz <141938389+segomezz@users.noreply.github.com>
2023-11-19 08:29:22 -0500

    Adicion de comentarios

--- src/uiMain/main.py
+++ src/uiMain/main.py
@@ -25,7 +25,6 @@ class Main:
             self.cambio = 0
         self.label_foto1.config(image=self.listaImagenesBienvenida[self.cambio])
         self.cambio += 1
-        print(self.cambio)
         if self.cambio >= len(self.listaImagenesBienvenida):
             self.cambio = 0
             
@@ -33,14 +32,20 @@ class Main:
     def ayuda(self):
         messagebox.showinfo("Creadores", "Miguel Angel Peña\nJuan Pablo Gaviria\nSebastian Gomez\nJuan Felipe Cadavid")
 
+    #Evento cambio de ventana
+
     def cambioDeVentana(self):
         self.ventana.withdraw()
         self.ventanaPrincipalI.deiconify()
+
+    #Evento volver ventana
 
     def volverVentana(self):
         self.ventanaPrincipalI.withdraw()
         self.ventana.deiconify()
 
+    #Evento cambio de contenido en la ventana interfaz usuario
+    
     def cambiarContenido(self, proceso):
         if proceso == "Compra":
             self.LabelDesc.config(text="Realizar Compra")
@@ -57,12 +62,16 @@ class Main:
             self.LabelDesc.config(text="Estadística")
             self.LabelDesc2.config(text=" Aquí va la descripción para ver estadísticas.")
 
+    #Carga de Imagenes para el CV
+
     def cargar_imagenes(self):
         tempList = []
         for i in range(0, len(self.listaCv)):
             tempList.append(ImageTk.PhotoImage(Image.open(f"src/{i + 1}.gif").resize((250, 225))))
         return tempList
     
+   #Carga de Imagenes Ecommerce
+   
     def cargar_imagenesBienvenida(self):
         tempListB = []
         for i in range(0, 5):
@@ -101,10 +110,15 @@ class Main:
 #------------------------------------------------------------------------------------------------
 
     def __init__(self):
+        
+        #Establecimiento de Ventana Principal
+        
         self.ventana = tk.Tk()
         self.ventana.attributes('-fullscreen', True)
         self.ventana.title("Choopi")
         self.ventana.geometry("600x500")
+
+        #Lista con los CV
 
         self.listaCv = [
             "Sebastian Gomez Zapata, 22 años, estudiante de la Universidad Nacional de Colombia sede Medellin de Ingenieria de Sistemas",
@@ -113,6 +127,7 @@ class Main:
             "Juan Felipe Cadavid Ruiz, estudiante de Ingenieria de Sistemas e Informatica, Universidad Nacional de Colombia"
         ]
 
+        #Se cargan las imagenes que se van a utilizar
         self.listaImagenesCv = self.cargar_imagenes()
         self.listaImagenesBienvenida = self.cargar_imagenesBienvenida()
 
@@ -146,12 +161,12 @@ class Main:
 
         self.p2_label_frame = tk.Frame(self.p2, bg="white")
         self.p2_label_frame.pack(side="top", padx=10, pady=10,fill="both", expand=True)
-        self.label_p2 = tk.Label(self.p2_label_frame, text=self.listaCv[0], font=("italic", 15), wraplength=270)
+        self.label_p2 = tk.Label(self.p2_label_frame, text=self.listaCv[0], font=("italic", 25), bg="white",wraplength=490)
         self.label_p2.grid(row=0, column=0) 
         self.p2_label_frame.grid_rowconfigure(0, weight=1)
         self.p2_label_frame.grid_columnconfigure(0, weight=1)
         self.label_p2.bind("<Button-1>", lambda event: self.CV())
-        self.p2_fotos_frame = tk.Frame(self.p2, bg="white")
+        self.p2_fotos_frame = tk.Frame(self.p2, bg="black")
         self.p2_fotos_frame.pack(pady=10,padx=10,fill="both",expand=True)
 
         img = Image.open("src/1.gif")
@@ -171,6 +186,7 @@ class Main:
 
         pil4 = ImageTk.PhotoImage(resize4)
 
+
         self.label_p2_fotos = tk.Label(self.p2_fotos_frame, image=pil, )
         self.label_p2_fotos.grid(row=0, column=0, pady=5, padx=5, sticky="news")
         self.label_p3_fotos = tk.Label(self.p2_fotos_frame, image=pil2, )
@@ -187,7 +203,7 @@ class Main:
         #FRAME BIENVENIDA
         self.p3 = tk.Frame(self.p1, bg="light blue")
         self.p3.pack(side="top", padx=10, pady=10, fill="both", expand=True)
-        self.label_p3 = tk.Label(self.p3, text="¡Bienvenido a Choopi!", font=("italic", 20))
+        self.label_p3 = tk.Label(self.p3, text="¡Bienvenido a Choopi!", font=("italic", 55),bg="light blue",fg="black")
         self.label_p3.pack(pady=10)
 
         self.p4 = tk.Frame(self.p1, bg="light blue")
@@ -200,12 +216,11 @@ class Main:
         self.label_foto1.pack(pady=10, padx=10)
         botonVentanaUsuario = tk.Button(self.p4, text="usuario interfaz", padx=60, pady=45, command=self.cambioDeVentana,bg="light blue",fg="black",  font=("Arial", 16),relief="raised", borderwidth=3,)
         botonVentanaUsuario.pack(side="bottom")
-        self.label_foto1.bind("<Enter>", lambda event: self.cambiar_imagenBienvenida())
+        self.label_foto1.bind("<Enter>", lambda event: self.cambiar_imagenBienvenida())   #Evento cambio de imagen Frame Bienvenida
 
-        #Evento cambio de imagen Frame Bienvenida
+     
+        #Ventana de Usuario Interfaz
         
-        #self.label_foto.bind("<Enter>", lambda event: self.())
-
         self.ventanaPrincipalI = tk.Toplevel()
         self.ventanaPrincipalI.title("choopi")
         self.ventanaPrincipalI.geometry("600x500")
@@ -213,12 +228,14 @@ class Main:
         self.menuP = tk.Menu(self.ventanaPrincipalI, tearoff=0)
         self.ventanaPrincipalI.config(menu=self.menuP)
 
+        #Menu archivo
         self.menuArchivo = tk.Menu(self.menuP, font=("italic"), tearoff=0)
         self.menuP.add_cascade(label="Archivo", menu=self.menuArchivo)
         self.menuArchivo.add_command(label="Aplicacion")
         self.menuArchivo.add_separator()
         self.menuArchivo.add_command(label="Salir", command=self.volverVentana)
 
+        #Menu funcionalidades
         self.MenuFuncionalidades = tk.Menu(self.menuP, font=("italic"), tearoff=0)
         self.menuP.add_cascade(label="Procesos y consultas", menu=self.MenuFuncionalidades)
         self.MenuFuncionalidades.add_command(label="Realizar Compra", command=lambda: self.cambiarContenido("Compra"))
@@ -227,7 +244,7 @@ class Main:
         self.MenuFuncionalidades.add_command(label="Estadistica", command=lambda: self.cambiarContenido("Estadistica"))
         self.menuP.add_command(label="ayuda", command=self.ayuda)
 
-        self.frameContenedor = tk.Frame(self.ventanaPrincipalI, bg="blue")
+        self.frameContenedor = tk.Frame(self.ventanaPrincipalI, bg="light blue")
         self.frameContenedor.pack(fill="both", expand=True)
 
         
