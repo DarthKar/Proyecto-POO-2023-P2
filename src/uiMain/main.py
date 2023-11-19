324186acc1a5291b46487027577248c66eb527ae
segomezz <141938389+segomezz@users.noreply.github.com>
2023-11-18 19:46:55 -0500

    Ventana de Inicio

--- src/uiMain/main.py
+++ src/uiMain/main.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from tkinter import messagebox
 from PIL import Image, ImageTk
 from field_frame import FieldFrame
-from field_frame_comprador_principal import field_frame_comprador_principal
 
 
 
@@ -35,8 +34,7 @@ class Main:
     def cambiarContenido(self, proceso):
         if proceso == "Compra":
             self.LabelDesc.config(text="Realizar Compra")
-            self.LabelDesc2.config(text="El sistema de gestión de carrito de compras permite a los usuarios agregar, eliminar y gestionar los productos que desean comprar mientras navegan por la tienda en línea. Esta funcionalidad involucra múltiples objetos y componentes que interactúan para proporcionar una experiencia de compra fluida y coherente.",font = ("arial",10),wraplength=500)
-            self.comprar()
+            self.LabelDesc2.config(text="Aquí va la descripción para realizar una compra.")
         elif proceso == "Devolucion":
             self.LabelDesc.config(text="Realizar Devolución")
             self.LabelDesc2.config(text="Aquí va la descripción para realizar una devolución.")
@@ -53,8 +51,6 @@ class Main:
         for i in range(0, len(self.listaCv)):
             tempList.append(ImageTk.PhotoImage(Image.open(f"src/{i + 1}.gif").resize((200, 356))))
         return tempList
-    
-#------------------------------------------------------------------------------------------------
 
     def opinion(self):
         for widget in self.FrameWidgets.winfo_children():
@@ -62,29 +58,6 @@ class Main:
         # Llama a FieldFrame con el número de campos deseado (por ejemplo, 3 campos)
         FieldFrame(self.FrameWidgets, "Datos",["ID", "Comentario", "Valoracion"], 3, "Introduce aqui los datos")
 
-#------------------------------------------------------------------------------------------------
-
-    def comprar(self):
-        for widget in self.FrameWidgets.winfo_children():
-            widget.destroy()
-        opciones = [
-    "Mostrar lista de productos",
-    "Agregar productos recomendados al carrito",
-    "Agregar productos al carrito",
-    "Eliminar productos del carrito",
-    "Mostrar carrito",
-    "Modificar carrito",
-    "Modificar informacion de pago",
-    "Crear orden de compra",
-    "Realizar pago",
-    "Vaciar ordenes de pago",
-    "Ver ordenes de pago",
-    "Volver al menu principal"
-    ]
-
-        field_frame_comprador_principal(self.FrameWidgets,"Opciones",opciones,12,"Numero de referencia")
-        
-#------------------------------------------------------------------------------------------------
 
     def __init__(self):
         self.ventana = tk.Tk()
@@ -140,26 +113,34 @@ class Main:
         self.p2_fotos_frame.pack(pady=10,padx=10,fill="both",expand=True)
 
         img = Image.open("src/1.gif")
-        resized_img = img.resize((125, 150))
+        resized_img = img.resize((250, 225))
         pil = ImageTk.PhotoImage(resized_img)
         img2 = Image.open("src/2.gif")
+        resize2 = img2.resize((250, 225))
 
-        pil2 = ImageTk.PhotoImage(img2)
+        pil2 = ImageTk.PhotoImage(resize2)
         img3 = Image.open("src/3.gif")
+        resize3 = img3.resize((250, 225))
 
-        pil3 = ImageTk.PhotoImage(img3)
+        pil3 = ImageTk.PhotoImage(resize3)
+        
         img4 = Image.open("src/4.gif")
+        resize4 = img4.resize((250, 225))
 
-        pil4 = ImageTk.PhotoImage(img4)
+        pil4 = ImageTk.PhotoImage(resize4)
 
         self.label_p2_fotos = tk.Label(self.p2_fotos_frame, image=pil, )
-        self.label_p2_fotos.grid(row=4, column=0, pady=5, padx=5, sticky="news")
-        self.label_p3_fotos = tk.Label(self.p2_fotos_frame, image=pil, )
-        self.label_p3_fotos.grid(row=3, column=0, pady=5, padx=5, sticky="news")
-        self.label_p4_fotos = tk.Label(self.p2_fotos_frame, image=pil, )
-        self.label_p4_fotos.grid(row=4, column=1, pady=5, padx=5, sticky="news")
-        self.label_p5_fotos = tk.Label(self.p2_fotos_frame, image=pil, )
-        self.label_p5_fotos.grid(row=3, column=1, pady=5, padx=5, sticky="news")
+        self.label_p2_fotos.grid(row=0, column=0, pady=5, padx=5, sticky="news")
+        self.label_p3_fotos = tk.Label(self.p2_fotos_frame, image=pil2, )
+        self.label_p3_fotos.grid(row=1, column=0, pady=5, padx=5, sticky="news")
+        self.label_p4_fotos = tk.Label(self.p2_fotos_frame, image=pil3, )
+        self.label_p4_fotos.grid(row=0, column=1, pady=5, padx=5, sticky="news")
+        self.label_p5_fotos = tk.Label(self.p2_fotos_frame, image=pil4, )
+        self.label_p5_fotos.grid(row=1, column=1, pady=5, padx=5, sticky="news")
+        self.p2_fotos_frame.grid_rowconfigure(0, weight=1)
+        self.p2_fotos_frame.grid_columnconfigure(0, weight=1)
+        self.p2_fotos_frame.grid_rowconfigure(1, weight=1)
+        self.p2_fotos_frame.grid_columnconfigure(1, weight=1)
 
         #FRAME BIENVENIDA
         self.p3 = tk.Frame(self.p1, bg="light blue")
@@ -170,10 +151,10 @@ class Main:
         self.p4 = tk.Frame(self.p1, bg="light blue")
         self.p4.pack(padx=10, pady=10, fill="both", expand=True)
         self.label_foto1 = tk.Label(self.p4)
-       # foto1 = Image.open("src/EcommerceImage.gif").resize((200, 356))
-        #foto1 = ImageTk.PhotoImage(foto1)
-        #self.label_foto1.config(image=foto1, compound="bottom")
-        #self.label_foto1.image = foto1
+        foto1 = Image.open("src/EcommerceImage.gif").resize((400, 356))
+        foto1 = ImageTk.PhotoImage(foto1)
+        self.label_foto1.config(image=foto1, compound="bottom")
+        self.label_foto1.image = foto1
         self.label_foto1.pack(pady=10, padx=10)
         botonVentanaUsuario = tk.Button(self.p4, text="usuario interfaz", padx=60, pady=45, command=self.cambioDeVentana,bg="light blue",fg="black",  font=("Arial", 16),relief="raised", borderwidth=3,)
         botonVentanaUsuario.pack(side="bottom")
@@ -187,7 +168,7 @@ class Main:
         imagenEcommerce5 =Image.open("src/EcommerceImage5.gif")
         pilEc5 = ImageTk.PhotoImage(imagenEcommerce5)
         index = 0
-        self.label_foto = tk.Label(self.p1)
+        self.label_foto = tk.Label(self.p1,)
         self.label_foto.config(image=pilEc2, compound="bottom")
         self.label_foto.image=pilEc2
         #self.label_foto.bind("<Enter>", lambda event: self.cambiar_imagen())
